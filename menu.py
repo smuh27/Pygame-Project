3583adb74a40d84b31c6219477cdd9f2aa667fea
Samir Muhammad <63614993+smuh27@users.noreply.github.com>
2024-03-05 20:28:19 -0500

    Question/Answer Screen

--- menu.py
+++ menu.py
@@ -9,9 +9,8 @@ start_img = pygame.image.load("images/start_btn.png").convert_alpha()
 exit_img = pygame.image.load("images/exit_btn.png").convert_alpha()
 pygame.display.set_caption("Trivia")
 
-
+player_name = ''
 answer_key = {'Kashif was the true creator of the server': False}
-
 
 class Button():
     def __init__(self, x, y, image, scale):
@@ -40,8 +39,9 @@ class Button():
 start_button = Button(100, 300, start_img, 0.8)
 exit_button = Button(500, 300, exit_img, 0.8)
 
+pygame.init()
+
 def main_menu():
-    pygame.init()
     font = pygame.font.Font('freesansbold.ttf', 50)
     text = font.render('Trivia', True, BLUE)
     textRect = text.get_rect()
@@ -54,7 +54,7 @@ def main_menu():
                 running = False
         screen.fill((202, 228, 241))
         if start_button.draw(screen):
-            stage1(screen)
+            stage1()
         if exit_button.draw(screen):
             running = False
         screen.blit(text, textRect)
@@ -64,7 +64,7 @@ def main_menu():
 
 
 
-def stage1(screen):
+def stage1():
     font = pygame.font.Font('freesansbold.ttf', 32)
     name_text = font.render('Enter Your Name: ', True, BLUE, )
     textRect = name_text.get_rect()
@@ -75,6 +75,7 @@ def stage1(screen):
     active = False
     color_active = pygame.Color('lightskyblue3')
     color_passive = pygame.Color('chartreuse4')
+    player_1 = ''
     color = color_passive
     text = ''
     running = True
@@ -93,8 +94,9 @@ def stage1(screen):
                 else:
                     text += event.unicode
             if event.type == pygame.KEYDOWN:
-                if event.type == pygame.K_RETURN:
-                    print(Player(text).name)
+                if event.key == pygame.K_RETURN:
+                    player_1 = Player(text)
+                    stage2(player_1)
         screen.fill((202, 228, 241))
 
         if active: 
@@ -107,23 +109,52 @@ def stage1(screen):
         screen.blit(text_surface, input_box)
         input_box.w = max(30, text_surface.get_width()+10)
         
+        pygame.display.update()
+
+
+def stage2(player_name):
+    font = pygame.font.Font('freesansbold.ttf', 24)
+    question = font.render(str(next(iter(answer_key))), True, BLUE, )
+    textRect = question.get_rect()
+    textRect.center = (WIDTH // 2, HEIGHT//4)
+    font = pygame.font.Font('freesansbold.ttf', 24)
+    answer_1 = font.render('True', True, BLUE, )
+    answer1Rect = answer_1.get_rect()
+    answer1Rect.center = (100, 300)
+    font = pygame.font.Font('freesansbold.ttf', 24)
+    answer_2 = font.render('False', True, BLUE, )
+    answer2Rect = answer_1.get_rect()
+    answer2Rect.center = (700, 300)
+    running = True
+    correct_answer = answer_key[str(next(iter(answer_key)))]
+    user_answer = None
+    while running:
+        screen.fill((202, 228, 241))
+
+
+        for event in pygame.event.get():
+            if event.type ==pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    if answer1Rect.collidepoint(event.pos):
+                        user_answer = True
+                        print("Incorrect")
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    if answer2Rect.collidepoint(event.pos):
+                        user_answer = False
+                        print("Correct")
+        screen.blit(question, textRect)
+        screen.blit(answer_1, answer1Rect)
+        screen.blit(answer_2, answer2Rect)
+
         pygame.display.flip()
 
-
-# def stage2(screen):
-#     running = True
-#     while running:
-#         for event in pygame.event.get():
-#             if event.type ==pygame.QUIT:
-#                 running = False
-#         screen.fill((202, 228, 241))
-#         font = pygame.font.Font('freesansbold.ttf', 24)
-#         text = font.render(str(next(iter(answer_key))), True, BLUE, )
-#         textRect = text.get_rect()
-#         textRect.center = (WIDTH // 2, HEIGHT//4)
-#         screen.blit(text, textRect)
+    if user_answer == correct_answer:
+        print("Correct")
+    else:
+        print("Incorrect")
 
 
-#         pygame.display.flip()
-
-#     pygame.quit()
+    pygame.quit()
--- player.py
+++ player.py
@@ -1,15 +1,11 @@
-
 class Player:
     def __init__(self, name):
-        self.name = name
         self.score = 0
+        self.name = name
+
 
     def set_score(self, score):
         self.score = score
     
     def increase_score(self, points):
         self.score += points 
-
-text = Player('Samir')
-
-print(text.name)
